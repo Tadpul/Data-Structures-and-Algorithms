cc8b9bbd6e28be859b01e386d8e0a0f99e8ae08a
Tadpul <david.bn.nartey@gmail.com>
2026-03-12 22:25:06 +0000

    added hash tables

--- Hash Tables/HashTables.cpp
+++ Hash Tables/HashTables.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 class Node
 {
@@ -18,21 +19,103 @@ private:
     Node* dataMap[SIZE];
 
 public:
+    HashTable()
+    {
+        for (int i{ 0 }; i < SIZE; i++)
+        {
+            dataMap[i] = nullptr;
+        }
+    }
+
     void printHashTable()
     {
         for (int i{ 0 }; i < SIZE; i++)
         {
-            std::cout << 1 << ": ";
             if (dataMap[i])
             {
                 Node* temp = dataMap[i];
+                std::cout << i << ':';
                 while (temp)
                 {
-                    std::cout << "{ " << temp->key << ", " << temp->value << " }\n";
+                    std::cout << " { " << temp->key << ", " << temp->value << " }";
                     temp = temp->next;
                 }
-                
+                std::cout << '\n';
             }
+            else std::cout << i << ": empty\n"; 
         }
     }
+
+    int hash(std::string key)
+    {
+        int hash{ 0 };
+        for (char letter : key)
+        {
+            int asciiValue{ int(letter) };
+            hash = (hash + int(asciiValue) * 23) % SIZE;
+        }
+        return hash;
+    }
+
+    void setNode(std::string key, int val)
+    {
+        Node* newNode(new Node(key, val));
+        int index{ hash(key) };
+
+        if (dataMap[index] == nullptr) dataMap[index] = newNode;
+        else
+        {
+            Node* temp{ dataMap[index] };
+            while (temp->next != nullptr)
+            {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+
+    int getValue(std::string key) 
+    {
+        int index = hash(key);
+        if (dataMap[index])
+        {
+            Node* temp{dataMap[index]};
+            while (temp)
+            {
+                if (temp->key == key) return temp->value;
+                temp = temp->next;
+            }
+        }
+        return INT_MIN;
+    }
+
+    std::vector<std::string> keys()
+    {
+        std::vector<std::string> keys;
+
+        for (Node* node : dataMap)
+        {
+            Node* temp{ node }; 
+            while (temp) 
+            {
+                keys.push_back(temp->key);
+                temp = temp->next;
+            }
+        }
+        return keys;
+    }
 };
+
+int main()
+{
+    HashTable myHT;
+
+    myHT.setNode("dildos", 10);
+    myHT.setNode("dildos", 2);
+    myHT.printHashTable();
+
+    for (std::string key : myHT.keys())
+    {
+        std::cout << key << " ";
+    }
+}
